7d0e85b49b53ae165314c45da5d303cea1596aaf
Yuliya <darkwader@mail.ru>
2023-02-11 14:47:33 +0300

    modified:   Example007_Magic/Program.cs~

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Clear();
 
-int xa = 30, ya = 10, xb = 40, yb = 1, xc = 50, yc = 10;
+int xa = 30, ya = 10, xb = 40, yb = 5, xc = 50, yc = 10;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -15,7 +15,7 @@ int x = xa, y = xb;
 
 int count = 0;
 
-while (count < 100)
+while (count < 10000)
 {
     int what = new Random().Next(0, 3); // [0;3) 0 1 2
     if (what == 0)
@@ -36,8 +36,8 @@ while (count < 100)
         y = (y + yc) / 2;
     }
 
-Console.SetCursorPosition(x, y); 
-Console.Write("+");
-count = count + 1; 
+    Console.SetCursorPosition(x, y); 
+    Console.WriteLine("+");
+    count = count + 1; 
 
 }
