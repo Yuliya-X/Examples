32d88b9eefbc2b1e9941784d740c6142ce27c648
Yuliya <darkwader@mail.ru>
2023-02-11 14:09:17 +0300

    Example007_Magic

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+int xa = 40, ya = 1, xb = 44, yb = 1, xc = 45, yc = 2;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.Write("+");
