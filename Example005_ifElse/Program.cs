f45285f75f70f2425f3ae5519a079fd5c7e386cd
Yuliya <darkwader@mail.ru>
2023-02-11 13:07:05 +0300

    Example005_ifElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "вова")
+{
+    Console.WriteLine("Ура, это Вовочка!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
